b9a756c7064fe664d3ccaa4449728f63951e8428
Omer Fisher <omerfisher04@gmail.com>
2024-08-18 14:13:11 +0300

    Division into files

---
+++ internet_layer.py
@@ -0,0 +1,7 @@
+def handle_3_layer(pkt, type_code):
+    """
+    Handles a third layer packet.
+    :param pkt: The packet in a bytes array format.
+    :param type_code: The type of the layer's protocol.
+    """
+    NotImplemented
